89b3a10c9c2e616f8d758a9c274e5b3cb9097079
Alok Yadav <yalok6321@gmail.com>
2024-11-07 07:01:56 +0530

    add a different api service for data

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
-const generateBtn = document.querySelector('.generate-btn')
-const quote = document.querySelector('.quote p')
-const author = document.querySelector('.author p')
-const error = document.querySelector('.error')
+const generateBtn = document.querySelector(".generate-btn");
+const quote = document.querySelector(".quote p");
+const author = document.querySelector(".author p");
+const error = document.querySelector(".error");
 
-const soundBtn = document.querySelector('#sound')
-const copyBtn = document.querySelector('#copy')
-const shareOnTwitter = document.querySelector('#twitter')
+const soundBtn = document.querySelector("#sound");
+const copyBtn = document.querySelector("#copy");
+const shareOnTwitter = document.querySelector("#twitter");
 
-const fetchQuoteUrl = 'https://api.quotable.io/random'
+const fetchQuoteUrl =
+	"https://api.freeapi.app/api/v1/public/quotes/quote/random";
 
-generateBtn.addEventListener('click', displayQuote)
+generateBtn.addEventListener("click", displayQuote);
 
-async function fetchQuote () {
-    generateBtn.classList.add('loading')
-    generateBtn.innerHTML = 'Loading Quote...'
-    const reponse = await fetch(fetchQuoteUrl)
-    
-    if (!reponse.ok) {
-        throw new Error('Failed to fetch Quote data.')
-    }
-    generateBtn.innerHTML = 'Inspire Me!'
-    generateBtn.classList.remove('loading')
-    return await reponse.json()
+async function fetchQuote() {
+	generateBtn.classList.add("loading");
+	generateBtn.innerHTML = "Loading Quote...";
+	const response = await fetch(fetchQuoteUrl);
+    console.log(response);
+	if (!response.ok) {
+		throw new Error("Failed to fetch Quote data.");
+	}
+	generateBtn.innerHTML = "Inspire Me!";
+	generateBtn.classList.remove("loading");
+
+	const data = await response.json();
+    console.log(data);
+	return data;
 }
 
-async function displayQuote () {
-    try {
-        const data = await fetchQuote();
-        quote.innerHTML = `${data.content}`
-        author.innerHTML = `${data.author}`
-        error.innerHTML = ""
-    } catch (error) {
-        error.innerHTML = `Please try again later : ${error}`
-    }
+async function displayQuote() {
+	try {
+		const response = await fetchQuote();
+		console.log(response);
+		quote.innerHTML = `${response.data.content}`;
+		author.innerHTML = `${response.data.author}`;
+		error.innerHTML = "";
+	} catch (error) {
+		error.innerHTML = `Please try again later : ${error}`;
+	}
 }
-displayQuote()
+displayQuote();
 
-soundBtn.addEventListener('click', () => {
-    let utterance = new SpeechSynthesisUtterance(`${quote.innerText} by ${author.innerText}`)
-    speechSynthesis.speak(utterance )
-})
+soundBtn.addEventListener("click", () => {
+	let utterance = new SpeechSynthesisUtterance(
+		`${quote.innerText} by ${author.innerText}`
+	);
+	speechSynthesis.speak(utterance);
+});
 
-copyBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText(quote.innerText)
-})
+copyBtn.addEventListener("click", () => {
+	navigator.clipboard.writeText(quote.innerText);
+});
 
-shareOnTwitter.addEventListener('click', () => {
-    let tweetUrl = `https://twitter.com/intent/tweet?url=${quote.innerText}`
-    window.open(tweetUrl, "_blank")
-})
+shareOnTwitter.addEventListener("click", () => {
+	let tweetUrl = `https://twitter.com/intent/tweet?url=${quote.innerText}`;
+	window.open(tweetUrl, "_blank");
+});
 
-window.setInterval(displayQuote, 30000)
+window.setInterval(displayQuote, 30000);
